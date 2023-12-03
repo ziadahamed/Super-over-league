32f97c22c30369fc96323a35fbe957da6e94597a
ziadahamed <ziadahamed17@gmail.com>
2023-12-03 08:07:19 +0000

    super

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  //   Write your code here.
+  <div className="bgContainer">
+    <h1 className="heading">Super Over League</h1>
+    <div className="imageContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="image"
+      />
+
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
